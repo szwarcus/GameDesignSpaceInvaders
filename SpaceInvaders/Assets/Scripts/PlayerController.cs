cc1fae82f75d7b0bcda5d7605fa56644e553c0ad
JakubSzwarc <szwarcus1994@gmail.com>
2018-11-05 16:59:34 +0100

    Added sliding effect and bouncing bounders. Fixed score manager.

--- SpaceInvaders/Assets/Scripts/PlayerController.cs
+++ SpaceInvaders/Assets/Scripts/PlayerController.cs
@@ -1,38 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     private Transform player;
     private float nextFire;
+    private Rigidbody2D rigid;
 
     public float speed;
     public float maxBound, minBound;
     public GameObject shot;
     public Transform shotSpawn;
     public float fireRate;
+
        
 	// Use this for initialization
 	void Start ()
     {
         player = GetComponent<Transform>();
+        rigid = GetComponent<Rigidbody2D>();
 	}
 	
 	void FixedUpdate ()
     {
         float h = Input.GetAxis("Horizontal");
 
-        if (player.position.x < minBound && h < 0)
+        if (player.position.x < minBound)
         {
             h = 0;
+            rigid.velocity *= -1;
+            return;
         }
-        else if (player.position.x > maxBound && h > 0)
+        else if (player.position.x > maxBound)
         {
             h = 0;
+            rigid.velocity *= -1;
+            return;
         }
+        float fHorizontalVelocity = rigid.velocity.x;
 
-        player.position += Vector3.right * h * speed;
+        fHorizontalVelocity += h;
+        if(Mathf.Abs(h)<0.01f)
+            fHorizontalVelocity *= Mathf.Pow(1f - 0.00001f, Time.deltaTime * 10f);
+        else if (Mathf.Sign(h) !=Mathf.Sign(fHorizontalVelocity))
+                fHorizontalVelocity *= Mathf.Pow(1f - 0.009f, Time.deltaTime * 10f);
+        else
+            fHorizontalVelocity *= Mathf.Pow(1f - 0.3f, Time.deltaTime * 10f);
+        rigid.velocity = new Vector2(fHorizontalVelocity, rigid.velocity.y);
+        //player.position += Vector3.right * h * speed;
 	}
 
     private void Update()
